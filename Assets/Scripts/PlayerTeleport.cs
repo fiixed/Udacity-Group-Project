a0910d694bb0efa4dd0ebe7f4830f991bfe6f5ca
Andrew Bell <fiixed@gmail.com>
2017-04-02 12:49:06 -0500

    feat:  basic GVR scene setup

---
+++ Assets/Scripts/PlayerTeleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour {
+
+	void Update() {
+		if (GvrController.ClickButtonUp) {
+			GvrLaserPointerImpl laserPointerImpl = (GvrLaserPointerImpl)GvrPointerManager.Pointer;
+			if (laserPointerImpl.IsPointerIntersecting) {
+				gameObject.transform.position = new Vector3(laserPointerImpl.PointerIntersection.x, gameObject.transform.position.y, laserPointerImpl.PointerIntersection.z);
+			}
+		}
+	}
+}
