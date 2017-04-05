9a5032973e2ddbd7255cedbb9c2627fde885cd9e
Andrew Bell <fiixed@gmail.com>
2017-04-05 17:26:10 -0500

    fix:  added android check for GVRController

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -9,13 +9,15 @@ public class PlayerTeleport : MonoBehaviour {
 
 	void Update() {
 		if (VRDevice.model == "Google, Inc. - Daydream View") {
+#if UNITY_HAS_GOOGLEVR && (UNITY_ANDROID || UNITY_EDITOR)
 			if (GvrController.ClickButtonUp) {
 				GvrLaserPointerImpl laserPointerImpl = (GvrLaserPointerImpl)GvrPointerManager.Pointer;
 				if (laserPointerImpl.IsPointerIntersecting) {
 					gameObject.transform.position = new Vector3(laserPointerImpl.PointerIntersection.x, gameObject.transform.position.y, laserPointerImpl.PointerIntersection.z);
 				}
 			}
+#endif  // UNITY_HAS_GOOGLEVR && (UNITY_ANDROID || UNITY_EDITOR)
 		}
-		
+
 	}
 }
