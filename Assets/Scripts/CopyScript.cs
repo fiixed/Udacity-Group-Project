f8df2767daeb2a9aae129ecd1cef469b5bf4a637
Andrew Bell <fiixed@gmail.com>
2017-04-04 21:23:36 -0500

    feat:  photon networking implemented

---
+++ Assets/Scripts/CopyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyScript : Photon.MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (photonView.isMine) {
+			transform.position = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+			transform.rotation = GameObject.FindGameObjectWithTag("MainCamera").transform.rotation;
+		}
+	}
+}
--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerTeleport : MonoBehaviour {
 
 	void Update() {
